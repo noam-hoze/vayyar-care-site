8ea91f75125c1db5ea7700fe8f6f4cc737e699c6
noamhoze <noam.hoze@vayyar.com>
2025-04-14 19:42:48 -0400

    Go back to css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "@/styles/main.scss";
+import "@/styles/main.css";
 import "./globals.css";
 import MainLayout from "@/components/MainLayout";
 
