c71375ff6b5691a3f32dc2f7b939e7688735c252
noam-hoze <noam.hoze@gmail.com>
2025-08-28 16:55:53 -0400

    fix: correct modal animation and content centering

--- src/components/ProductSection/ProductSection.tsx
+++ src/components/ProductSection/ProductSection.tsx
@@ -65,123 +65,125 @@ const ProductSection: React.FC<ProductSectionProps> = ({
                 isCloserLookActive ? styles.modalActive : ""
             }`}
         >
-            <div className={`${styles.logoContainer} ${styles.fadeIn}`}>
-                <VayyarLogo />
-            </div>
+            <div className={styles.modalContent}>
+                <div className={`${styles.logoContainer} ${styles.fadeIn}`}>
+                    <VayyarLogo />
+                </div>
 
-            <div className={styles.imageContainer}>
+                <div className={styles.imageContainer}>
+                    {isCloserLookActive ? (
+                        <>
+                            {productDetails.map((tab, index) => (
+                                <div
+                                    key={tab.title}
+                                    className={`${styles.mediaWrapper} ${
+                                        index === activeTabIndex
+                                            ? styles.fadeIn
+                                            : styles.fadeOutImmediately
+                                    }`}
+                                >
+                                    {tab.mediaType === "image" ? (
+                                        <img
+                                            src={tab.mediaSrc}
+                                            alt={tab.title}
+                                            className={styles.productImage}
+                                        />
+                                    ) : (
+                                        <video
+                                            ref={(el) =>
+                                                (videoRefs.current[index] = el)
+                                            }
+                                            src={tab.mediaSrc}
+                                            className={styles.productImage}
+                                            autoPlay
+                                            muted
+                                            loop
+                                            playsInline
+                                        />
+                                    )}
+                                </div>
+                            ))}
+                        </>
+                    ) : (
+                        <div className={styles.mediaWrapper}>
+                            <img
+                                src="/images/product.png"
+                                alt="Vayyar Care Product"
+                                className={styles.productImage}
+                            />
+                        </div>
+                    )}
+                </div>
+
                 {isCloserLookActive ? (
-                    <>
-                        {productDetails.map((tab, index) => (
-                            <div
-                                key={tab.title}
-                                className={`${styles.mediaWrapper} ${
-                                    index === activeTabIndex
-                                        ? styles.fadeIn
-                                        : styles.fadeOutImmediately
-                                }`}
-                            >
-                                {tab.mediaType === "image" ? (
-                                    <img
-                                        src={tab.mediaSrc}
-                                        alt={tab.title}
-                                        className={styles.productImage}
-                                    />
-                                ) : (
-                                    <video
-                                        ref={(el) =>
-                                            (videoRefs.current[index] = el)
-                                        }
-                                        src={tab.mediaSrc}
-                                        className={styles.productImage}
-                                        autoPlay
-                                        muted
-                                        loop
-                                        playsInline
-                                    />
-                                )}
+                    <div className={`${styles.noamClass}`}>
+                        <div
+                            className={`${styles.detailsContainer} ${styles.fadeIn}`}
+                        >
+                            <div className={styles.tabsContainer}>
+                                {productDetails.map((tab, index) => (
+                                    <button
+                                        key={tab.title}
+                                        className={`${styles.tabButton} ${
+                                            index === activeTabIndex
+                                                ? styles.activeTab
+                                                : ""
+                                        }`}
+                                        onClick={() => setActiveTabIndex(index)}
+                                    >
+                                        {tab.title}
+                                        <motion.div
+                                            className={styles.activeTabLine}
+                                            animate={{
+                                                opacity:
+                                                    index === activeTabIndex
+                                                        ? 1
+                                                        : 0,
+                                            }}
+                                            transition={{ duration: 0.3 }}
+                                        />
+                                    </button>
+                                ))}
                             </div>
-                        ))}
-                    </>
+                            <AnimatePresence mode="wait">
+                                <motion.p
+                                    key={activeTab.description}
+                                    className={styles.descriptionText}
+                                    initial={{ opacity: 0 }}
+                                    animate={{ opacity: 1 }}
+                                    exit={{ opacity: 0 }}
+                                    transition={{ duration: 0.3 }}
+                                >
+                                    {activeTab.description}
+                                </motion.p>
+                            </AnimatePresence>
+                        </div>
+                    </div>
                 ) : (
-                    <div className={styles.mediaWrapper}>
-                        <img
-                            src="/images/product.png"
-                            alt="Vayyar Care Product"
-                            className={styles.productImage}
-                        />
+                    <div
+                        className={`${styles.buttonContainer} ${
+                            isCloserLookActive ? styles.fadeOut : styles.fadeIn
+                        }`}
+                    >
+                        <button
+                            className={styles.closerLookButton}
+                            onClick={handleToggleCloserLook}
+                        >
+                            <span className={styles.plusIcon}>+</span>
+                            <span>Take a closer look</span>
+                        </button>
                     </div>
                 )}
-            </div>
 
-            {isCloserLookActive ? (
-                <div className={`${styles.noamClass}`}>
-                    <div
-                        className={`${styles.detailsContainer} ${styles.fadeIn}`}
-                    >
-                        <div className={styles.tabsContainer}>
-                            {productDetails.map((tab, index) => (
-                                <button
-                                    key={tab.title}
-                                    className={`${styles.tabButton} ${
-                                        index === activeTabIndex
-                                            ? styles.activeTab
-                                            : ""
-                                    }`}
-                                    onClick={() => setActiveTabIndex(index)}
-                                >
-                                    {tab.title}
-                                    <motion.div
-                                        className={styles.activeTabLine}
-                                        animate={{
-                                            opacity:
-                                                index === activeTabIndex
-                                                    ? 1
-                                                    : 0,
-                                        }}
-                                        transition={{ duration: 0.3 }}
-                                    />
-                                </button>
-                            ))}
-                        </div>
-                        <AnimatePresence mode="wait">
-                            <motion.p
-                                key={activeTab.description}
-                                className={styles.descriptionText}
-                                initial={{ opacity: 0 }}
-                                animate={{ opacity: 1 }}
-                                exit={{ opacity: 0 }}
-                                transition={{ duration: 0.3 }}
-                            >
-                                {activeTab.description}
-                            </motion.p>
-                        </AnimatePresence>
-                    </div>
-                </div>
-            ) : (
-                <div
-                    className={`${styles.buttonContainer} ${
-                        isCloserLookActive ? styles.fadeOut : styles.fadeIn
-                    }`}
-                >
+                {isCloserLookActive && (
                     <button
-                        className={styles.closerLookButton}
+                        className={`${styles.closeButton} ${styles.fadeIn}`}
                         onClick={handleToggleCloserLook}
                     >
-                        <span className={styles.plusIcon}>+</span>
-                        <span>Take a closer look</span>
+                        <div className={styles.closeIcon}></div>
                     </button>
-                </div>
-            )}
-
-            {isCloserLookActive && (
-                <button
-                    className={`${styles.closeButton} ${styles.fadeIn}`}
-                    onClick={handleToggleCloserLook}
-                >
-                    <div className={styles.closeIcon}></div>
-                </button>
-            )}
+                )}
+            </div>
         </div>
     );
 };
